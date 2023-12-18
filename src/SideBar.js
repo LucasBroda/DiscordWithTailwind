fa61c26278b203fd2e37d9405a7bf207a9f487ac
lucas_broda <lucas_broda@ens.univ-artois.fr>
2023-12-18 19:08:38 +0100

    feat(SideBar): adding new icons

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,12 +2,18 @@ import SideBarIcon from "./SideBarIcon"
 import { GrAdd } from "react-icons/gr";
 import { DiAptana } from "react-icons/di";
 import { GoFileDirectory } from "react-icons/go";
+import { HiAnnotation } from "react-icons/hi";
 
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
             <SideBarIcon icon={<GrAdd size="28"/>}></SideBarIcon>
             <SideBarIcon icon={<GoFileDirectory size="28"/>}></SideBarIcon>
+            <SideBarIcon icon={<HiAnnotation size="28"/>}></SideBarIcon>
+            <SideBarIcon icon={<HiAnnotation size="28"/>}></SideBarIcon>
+            <SideBarIcon icon={<HiAnnotation size="28"/>}></SideBarIcon>
+            <SideBarIcon icon={<HiAnnotation size="28"/>}></SideBarIcon>
+            <SideBarIcon icon={<HiAnnotation size="28"/>}></SideBarIcon>
             <SideBarIcon icon={<DiAptana size="28"/>}></SideBarIcon>
         </div>
     )
